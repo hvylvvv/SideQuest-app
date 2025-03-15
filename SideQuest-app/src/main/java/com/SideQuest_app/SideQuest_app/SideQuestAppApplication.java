9fad04c1ab5b79ec192967dc9f9d95d400ef6888
Ajayyy-code <79764618+Ajayyy-code@users.noreply.github.com>
2025-03-15 12:26:23 -0500

    Initial project framework

---
+++ SideQuest-app/src/main/java/com/SideQuest_app/SideQuest_app/SideQuestAppApplication.java
@@ -0,0 +1,13 @@
+package com.SideQuest_app.SideQuest_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideQuestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideQuestAppApplication.class, args);
+	}
+
+}
---
+++ SideQuest-app/src/test/java/com/SideQuest_app/SideQuest_app/SideQuestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SideQuest_app.SideQuest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideQuestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
